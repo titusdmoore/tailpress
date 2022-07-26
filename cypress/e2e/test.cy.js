204f0202457ff453a095c1e59a5f15f215c6bf7b
Titus Moore <tmoore@edgewebware.com>
2022-07-26 16:52:12 -0400

    Added basic test, ran build

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,9 @@
-describe('empty spec', () => {
+describe('Testing WordPress', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('localhost:8181');
+
+    cy.contains('Blog').click();
+
+    cy.url().should('include', '/sample-page/');
   })
 })
--- flexible-content/build/type-tabs/index.asset.php
+++ flexible-content/build/type-tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'cc18dd998cfefbe51123');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '95f548c889d8709064ba');
