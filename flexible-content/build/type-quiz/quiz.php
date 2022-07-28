2498415f0a797856eecf2b07e6eddc8c1fac6f4c
titusdmoore <titusdmoore@gmail.com>
2022-07-28 13:35:51 -0400

    Trying to fix stuff

--- flexible-content/build/type-quiz/quiz.php
+++ flexible-content/build/type-quiz/quiz.php
@@ -2,7 +2,7 @@
 
 function render_quiz($attributes)
 {
-  wp_enqueue_script( "edgepressQuizFrontend", __DIR__ . '/quiz.js', array("wp-element"));
+  wp_enqueue_script( "edgepressQuizFrontend", get_template_directory(__FILE__) . '/quiz.js', array("wp-element"), true);
   ob_start(); ?>
   <div class="edgepress-quiz-block">
     <h1>Hello World</h1>
